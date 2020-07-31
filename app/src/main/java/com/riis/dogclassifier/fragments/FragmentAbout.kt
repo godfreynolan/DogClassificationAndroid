cefeacf9cca95529f706e907c235112ab735682a
unknown <reedyjw1@udmercy.edu>
2020-07-31 17:06:46 -0400

    added about tab

--- app/src/main/java/com/riis/dogclassifier/fragments/FragmentAbout.kt
+++ app/src/main/java/com/riis/dogclassifier/fragments/FragmentAbout.kt
@@ -1,9 +1,13 @@
 package com.riis.dogclassifier.fragments
 
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.riis.dogclassifier.R
 
@@ -13,6 +17,21 @@ class FragmentAbout(): Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val dataset = view.findViewById<TextView>(R.id.dataset)
+        val github = view.findViewById<TextView>(R.id.github)
+        dataset.autoLinkMask = 0
+        github.autoLinkMask = 0
+
+        val datasetHtml: Spanned = Html.fromHtml(getString(R.string.about_DataSet))
+        dataset.text = datasetHtml
+        dataset.movementMethod = LinkMovementMethod.getInstance()
+
+        val githubHtml: Spanned = Html.fromHtml(getString(R.string.about_GitHub))
+        github.text = githubHtml
+        github.movementMethod = LinkMovementMethod.getInstance()
+
+        return view
     }
 }
